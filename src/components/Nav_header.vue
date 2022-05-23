d369ec04a74fb8ae8466cc1ad9e67907399cfa5a
serdeljac <hi@stjepanerdeljac.com>
2022-05-22 20:05:12 -0700

    init build

---
+++ src/components/Nav_header.vue
@@ -0,0 +1,12 @@
+<template>
+  <header class="header">
+    <div class="menu_toggle">Menu</div>
+    <div class="menu_title">Learn SVG Helper</div>
+  </header>
+</template>
+
+<script>
+export default {
+  name: 'navigation-header',
+}
+</script>
---
+++ src/components/sidebar_menu.vue
@@ -0,0 +1,23 @@
+<template>
+    <div class="menu">
+
+        <ul>
+            <li>SVG canvas area</li>
+            <li>Basic Shapes</li>
+            <li>Using Text</li>
+            <li>Grouping</li>
+            <li>Fills and Strokes</li>
+            <li>Using Gradients</li>
+            <li>ClipPath and Masks</li>
+            <li>Add image into SVG</li>
+            <li>Recommended Software for making SVG's</li>
+        </ul>
+
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'sidebar_menu'
+    }
+</script>
